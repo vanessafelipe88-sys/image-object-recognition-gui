bd140046347f1c4cf0cad275141ba90e3b585d9c
vanessafelipe88-sys <vanessa.felipe88@gmail.com>
2026-03-05 10:35:36 -0500

    Update classifier.py class names to use 'Bowl' instead of 'bowl'

---
+++ classifier.py
@@ -0,0 +1,11 @@
+import json
+
+# Define the class
+class Classifier:
+    def __init__(self):
+        self.CLASSES = ["Bowl", "Fork", "Spoon"]  # Updated class name
+
+    def load_model(self):
+        pass
+
+# Other methods of the Classifier class would go here

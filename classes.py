bbab813d668e1550965eede5149ce158b7fece8f
vanessafelipe88-sys <vanessa.felipe88@gmail.com>
2026-03-05 10:40:18 -0500

    Create classes.py with definitions for RedManicure, File, and Bowl.

---
+++ classes.py
@@ -0,0 +1,11 @@
+class RedManicure:
+    def __init__(self, color="red"):
+        self.color = color
+
+class File:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+class Bowl:
+    def __init__(self, material):
+        self.material = material
